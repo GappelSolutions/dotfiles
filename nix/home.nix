beed081f8f5e8b635e6626ac63a4516d77c75a92
Christian Gappel <aichelberg2@gmail.com>
2026-03-02 10:33:54 +0100

    fix(direnv): expand home directory in whitelist prefix
    Replace literal $HOME (not expanded by direnv) with Nix-evaluated
    config.home.homeDirectory so the whitelist actually matches ~/dev/*.

--- nix/home.nix
+++ nix/home.nix
@@ -313,7 +313,7 @@ in
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    config.whitelist.prefix = [ "$HOME/dev" ];
+    config.whitelist.prefix = [ "${config.home.homeDirectory}/dev" ];
   };
 
   # FZF
